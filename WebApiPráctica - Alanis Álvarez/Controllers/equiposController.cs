b41840e0b3c90a617e3d3c10a7eb33bf72ede46f
Alanis3a1b <alanis.alvarez@catolica.edu.sv>
2024-02-20 22:43:02 -0600

    Agregar archivos de proyecto.

---
+++ WebApiPráctica - Alanis Álvarez/Controllers/equiposController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiPráctica___Alanis_Álvarez.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiPráctica___Alanis_Álvarez.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos
+                                           select e).ToList();
+
+            if (listadoEquipo.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoEquipo);
+        }
+
+    }
+
+
+
+}
---
+++ WebApiPráctica - Alanis Álvarez/Models/equiposContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace WebApiPráctica___Alanis_Álvarez.Models
+{
+    public class equiposContext : DbContext
+    {
+        public equiposContext(DbContextOptions<equiposContext> options) : base(options) 
+        { 
+        
+        }
+
+        public DbSet<equipos> equipos { get; set; }
+    }
+}

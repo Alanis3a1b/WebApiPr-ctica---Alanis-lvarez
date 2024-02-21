0d92eabaecb64978966feb01a3a91b3dd2297259
Alanis3a1b <alanis.alvarez@catolica.edu.sv>
2024-02-20 23:42:44 -0600

    Se agregaron los métodos de buscar por Id, por String, insertar y modificar registro de la guía practica #2

--- WebApiPráctica - Alanis Álvarez/Controllers/equiposController.cs
+++ WebApiPráctica - Alanis Álvarez/Controllers/equiposController.cs
@@ -17,6 +17,7 @@ namespace WebApiPráctica___Alanis_Álvarez.Controllers
             _equiposContexto = equiposContexto;
         }
 
+        //Método para leert todos los registros
         [HttpGet]
         [Route("GetAll")]
 
@@ -33,7 +34,88 @@ namespace WebApiPráctica___Alanis_Álvarez.Controllers
             return Ok(listadoEquipo);
         }
 
-        //Prueba enviado a repositorio 
+        //Método para buscar por ID
+        [HttpGet]
+        [Route("GetById/{id}")]
+
+        public IActionResult Get(int id) 
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+            
+            if (equipo == null) 
+            {
+                return NotFound();
+            
+            }
+
+            return Ok(equipo);
+        }
+
+        //Buscar por descripción
+        [HttpGet]
+        [Route("Find/{filtro}")]
+
+        public IActionResult Get(string filtro) 
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.descripcion.Contains(filtro)
+                               select e).FirstOrDefault();
+            if (equipo == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(equipo);
+        }
+
+        //Método para crear o insertar registros
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarEquipo([FromBody] equipos equipo)
+        {
+            try
+            {
+                _equiposContexto.equipos.Add(equipo);
+                _equiposContexto.SaveChanges();
+                return Ok(equipo);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        //Método para modificar los registros de la tabla
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar)
+        {
+            equipos? equipoActual = (from e in _equiposContexto.equipos
+                                     where e.id_equipos == id
+                                     select e).FirstOrDefault();
+
+            if (equipoActual == null) 
+            { return NotFound(); }
+
+            equipoActual.nombre = equipoModificar.nombre;
+            equipoActual.descripcion = equipoModificar.descripcion;
+            equipoActual.marca_id = equipoModificar.marca_id;
+            equipoModificar.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equipoModificar.anio_compra = equipoModificar.anio_compra;
+            equipoModificar.costo = equipoModificar.costo;
+
+            _equiposContexto.Entry(equipoActual).State = EntityState.Modified;
+            _equiposContexto.SaveChanges();
+
+            return Ok(equipoModificar);
+
+
+
+        }
 
     }
 
--- WebApiPráctica - Alanis Álvarez/Models/equipos.cs
+++ WebApiPráctica - Alanis Álvarez/Models/equipos.cs
@@ -8,6 +8,7 @@ namespace WebApiPráctica___Alanis_Álvarez.Models
         [Key]
 
         public int id_equipos { get; set; }
+        public string nombre { get; set; }
         public string descripcion { get; set; }
         public int? tipo_equipo_id { get; set; }
         public int? marca_id {  get; set; }
